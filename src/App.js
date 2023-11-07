218ca2218a37c653a8a15ca9143c0bec06bef951
Udaykalse <Udaysinhk.2000@gmail.com>
2023-11-08 00:37:41 +0530

    aap js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import Home from "./component/Home/Home";
-import Navbar from "./component/Navbar/Navbar";
-import About from "./component/About/About";
-import Join from "./component/Join/Joinus";
-import Shop from "./component/Shop/Shop";
-import Events from "./component/Events/Events";
-import Inde from "./component/FAQs/Inde";
-import Contact from "./component/Contact/Contact";
-import Footer from "./component/Footer/footer";
-
+import React from "react";
+import Editindex from './component/Pages/Registration/registrationFrom'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Join/>
-      <Shop/>
-      <Events/>
-      <Inde/>
-      <Contact/>
-      <Footer/>
-    </div>
+   <>
+  <Editindex/>
+   </>
   );
 }
 
