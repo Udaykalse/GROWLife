78dd6af6a9862e1bb5921710489fb612e86ba1b7
Udaykalse <Udaysinhk.2000@gmail.com>
2023-11-08 12:01:10 +0530

    Order details componet

---
+++ src/component/Pages/MainOder/OrderDetails/OderDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './../../NavbarPage/NavbarPage';
+
+const OrderDetails = () => {
+  return (
+    <div>
+      <Navbar title=" OrderDetails" />
+      2 Order
+    </div>
+  );
+}
+
+export default OrderDetails;
